ab87ea403fe4d8c0aec604a4eb6ec5194b64e209
zell992004 <zell992004@gmail.com>
2023-12-08 11:41:04 -0500

    init

---
+++ modules/core/user.nix
@@ -0,0 +1,39 @@
+{ pkgs
+, inputs
+, ...
+}:
+let
+  name = "Peter Bouchard";
+  username = "zell";
+  email = "zell";
+  initialPassword = "Newcomer2023";
+  packages = with pkgs; [
+    fish
+  ];
+in
+{
+  imports = [ inputs.home-manager.nixosModules.home-manager ];
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    extraSpecialArgs = { inherit inputs; };
+    users.${username} = {
+      imports = [ (import ./../home) ];
+      home.username = username;
+      home.homeDirectory = "/home/${username}";
+      home.stateVersion = "22.11";
+      programs.home-manager.enable = true;
+    };
+  };
+
+  users.users.${username} = {
+    isNormalUser = true;
+    initialPassword = initialPassword;
+    description = name;
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+  };
+  users.users.root.initialPassword = initialPassword;
+  # services.getty.autologinUser = "fahim";
+  nix.settings.allowed-users = [ "zell" ];
+}
