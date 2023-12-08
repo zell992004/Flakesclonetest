2ff42e036b13dd002d1e2b8995c2cd66ed6b577f
Peter Bouchard <zell992004@gmail.com>
2023-12-09 03:00:25 +0600

    updates

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -61,7 +61,6 @@ in
 
       decoration {
           rounding = 0
-          blur_new_optimizations = 1
           active_opacity = 0.8500;
           inactive_opacity = 0.76;
           drop_shadow = 0
