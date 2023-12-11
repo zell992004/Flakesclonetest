c6e66eb89d5410ef9b9fbc78dfa2b43f9d8c02d5
Peter Bouchard <zell992004@gmail.com>
2023-12-11 17:40:50 -0500

    adding pkgs to home2

--- modules/home2/default.nix
+++ modules/home2/default.nix
@@ -1,23 +1,23 @@
 { inputs, ... }: {
   imports =
     #[ (import ./waybar) ]
-      [ (import ./firefox) ];
+      [ (import ./firefox) ]
     # ++ [ (import ./vscode) ]
     #++ [ (import ./chromium) ]
     #++ [ (import ./hyprland) ]
     #++ [ (import ./gaming) ]
-    #++ [ (import ./git) ]
-    #++ [ (import ./foot) ]
+    ++ [ (import ./git) ]
+    ++ [ (import ./foot) ]
     #++ [ (import ./swaylock) ]
     #++ [ (import ./wofi) ]
-    #++ [ (import ./zsh) ]
-    #++ [ (import ./gtk) ]
+    ++ [ (import ./zsh) ]
+    ++ [ (import ./gtk) ]
     #++ [ (import ./mako) ]
     #++ [ (import ./scripts) ]
     #++ [ (import ./neofetch) ]
     #++ [ (import ./starship) ]
     #++ [ (import ./wlogout) ]
-    #++ [ (import ./nvim) ]
-    #++ [ (import ./package) ];
+    ++ [ (import ./nvim) ]
+    ++ [ (import ./package) ];
 
 }
